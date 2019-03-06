0341fdba2203199bf433546354764b4ea949380b
KhaDy308Nguyen <khady308nguyen@gmail.com>
2019-03-06 11:32:46 +0700

    Add home

--- PhongVuWeb/src/app/app.module.ts
+++ PhongVuWeb/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './home/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
