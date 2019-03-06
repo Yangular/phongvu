40f0561a8e03c3cfe21f110b16015b078bc1e3bd
KhaDy308Nguyen <khady308nguyen@gmail.com>
2019-03-06 16:57:29 +0700

    add form

--- PhongVuWeb/src/app/login/login.component.ts
+++ PhongVuWeb/src/app/login/login.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public clicklogin = true;
+  public click = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onclick () {
+    this.click = !this.click;
+    this.clicklogin = !this.clicklogin;
+  }
 }
